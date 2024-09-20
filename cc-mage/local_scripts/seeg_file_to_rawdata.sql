4962e381b9e5ace9feae128f1e727fe0aae78417
Amanda Eames <amandaeames@Amandas-MacBook-Pro.local>
2024-09-20 16:26:23 +0200

    feat added seeg pipeline for modelled emissions

--- cc-mage/local_scripts/seeg_file_to_rawdata.sql
+++ cc-mage/local_scripts/seeg_file_to_rawdata.sql
@@ -3,26 +3,15 @@
 -- This should be executed on DuckDB
 
 CREATE OR REPLACE TABLE seeg_city_emissions AS
-SELECT *
-FROM read_csv('/Users/<username>/Downloads/Dados por município 11.1*/*/gases.csv', header =True)
-WHERE "Setor de emissão" <> 'Setor de emissão'
-;
-
-CREATE OR REPLACE TABLE translation_table AS
-SELECT *
-FROM read_csv('/Users/<username>/Downloads/SEEG to GPC Mapping - DetailedMapping.csv', header = True)
-;
-
-CREATE OR REPLACE TABLE seeg_city_emissions_tr AS 
-SELECT 		"Emission sector" AS sector,
-			"Issuer category" AS category,
-			"Issuer sub-category" AS subcategory,
-			"Product or system" AS product_or_system,
-			'Detailing' AS details,
-			'Crop' AS crop,
-			"General activity" AS actvity,
-			'Biome' AS biome,
-			"Issuance/Removal/Bunker" AS emissions_type,
+SELECT 		"Setor de emissão" AS setor_de_emissão,
+			"Categoria emissora" AS categoria_emissora,
+			"Sub-categoria emissora" AS subcategoria_emissora,
+			"Produto ou sistema" AS produto_ou_sistema,
+			Detalhamento AS detalhamento,
+			Recorte AS recorte,
+			"Atividade geral" AS atividade_geral,
+			Bioma AS bioma,
+			"Emissão/Remoção/Bunker" AS emissão_remoção,
 			 replace("Gás", ' (t)', '') AS gas_name,
 			 'tonnes' as emissions_units,
 			 REGEXP_REPLACE(Cidade, '\s*\(.*?\)','') AS city,
@@ -35,23 +24,15 @@ SELECT 		"Emission sector" AS sector,
 			"2020" AS emissions_2020,
 			"2021" AS emissions_2021,
 			"2022" AS emissions_2022
-FROM 		seeg_city_emissions e
-LEFT JOIN 	translation_table s
-ON 			TRIM(e."Setor de emissão") = TRIM(s."Setor de emissão")
-AND 		TRIM(e."Categoria emissora") = TRIM(s."Categoria emissora")
-AND  		TRIM(e."Sub-categoria emissora") = TRIM(s."Sub-categoria emissora")
-AND 		TRIM(e."Produto ou sistema") = TRIM(s."Produto ou sistema")
-AND  		TRIM(e.Detalhamento) = TRIM(s.Detalhamento) 
-AND 		TRIM(e.Recorte) = TRIM(s.Recorte)
-AND 		TRIM(e."Atividade geral") = TRIM(s."Atividade geral")
-AND 		TRIM(e.Bioma) = TRIM(s.Bioma) 
-AND 		TRIM(e."Emissão/Remoção/Bunker") = TRIM(s."Emissão/Remoção/Bunker")
+FROM read_csv('/Users/amandaeames/Downloads/Dados por município 11.1*/*/gases.csv', header =True)
+WHERE "Setor de emissão" <> 'Setor de emissão'
+;
 
 -- This will output the table into a directory partitioned by the sector and as a parquet
 -- we need to use partitions and parquet format because the dataset is too large otherwise
 COPY (
     SELECT *
-    FROM seeg_city_emissions_tr
+    FROM seeg_city_emissions
 )
 TO '/Users/<username>/Documents/seeg_city_emissions/'
 WITH (
