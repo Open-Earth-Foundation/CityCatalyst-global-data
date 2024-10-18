9cb7c6a823dcd98a562f5a72c38d0fa78e40ec22
mfonsecaOEF <maureen@openearth.org>
2024-10-18 18:57:04 +0200

    chore:short methodology mapping

--- importer-creator/context/mappings/mappings_methodologies.py
+++ importer-creator/context/mappings/mappings_methodologies.py
@@ -1,13 +1,8 @@
 # context_methodologies.py
-
-# @ Mau: Please check the mapping and
-# how it is best to structure based on the information we have extracted until here like GPC number, activities and so on.
-# what names do we need to use for the keys e.g. input_values and so on so thetyt align with the activities mappings?
-
 methodologies_mapping = {
     "__doc__": """
     This is the dictionary of different methodologies for the 'Stationary Energy' sector and 'Transportation' sector.
-    It is used to provide context for choosing the correct methodology and for making transformations from activity data to emission values.
+    It is used to provide context for choosing the correct methodology based on the GPC (Global Protocol for Community-Scale Greenhouse Gas Emission Inventories) reference number and the activity units.
 
     The dictionary contains the following:
     4 methodologies as follows:
@@ -20,33 +15,21 @@ methodologies_mapping = {
     - sectors: The sector to which the methodology belongs
     - scope: The scope of the methodology. This indicates for which scopes the methodology is applicable
     - gpc_refno: The GPC reference numbers associated with the methodology. 
-    - input_values: The input values required for making the transformation.
-    - subcategories_i_j: The subcategories for the input values. For example what fuel type (i) and building type (j) for the input values total_fuel_consumption.
     - units: The units for the input values.
-    - default_value: The default emission factor value to be used for the calculation.
-    - default_units: The default units for the default emission factor value which is normally used for the calculation.
-    - formula: The formula to calculate the emissions from the activity data. It is encoded in LaTeX format.
+    - activity_name: The name of the activity to which the methodology is applicable.
     
-    E.g. if the activity data is related to fuel combustion consumption for 'Stationary Energy' sector and the identified GPC reference number is 'I.2.1', the fuel type (i) and building type (j) is needed. 
-    The activity value is typically in the units of TJ, kg, or m3.
-    According to the formula given in the methodology, the emissions can be calculated by multiplying the activity value for the fuel type (i) and the building type (j) with the default emission_factor (i).
-    This should result in emissions in 'kg'.
-
-    E.g. if the activity data is related to energy consumption (from the grid) for 'Stationary Energy' or 'Transportation' sector and the identified GPC reference number is 'II.1.2', the building type (j) or transport type (j) is needed.
-    The activity value is typically in the units of kWh.
-    According to the formula given in the methodology, the emissions can be calculated by multiplying the activity value for the building type (j) or transport type (j) with the default emission_factor (i).
-    This should result in emissions in 'kg'.
+    Examples:
+    - if the activity name is "fuel_combustion", the gpc_refno is 'II.1.1' and the units are 'TJ', 'kg', or 'm3', the methodology to be used is 'fuel_combustion_consumption'.
+    - if the activity name is "electricity_consumption", the gpc_refno is 'II.1.2' and the units are 'kWh', the methodology to be used is 'energy_consumption'.
+    - if the activity name is "fuel_combustion", the gpc_refno is 'I.1.1' and the units are 'TJ', 'kg', or 'm3', the methodology to be used is 'fuel_sales'.
+    - if the activity name is "vehicle_kilometers_traveled", the gpc_refno is 'II.2.1' and the units are 'pkm' or 'gkm', the methodology to be used is 'movement_driver'.
     """,
     "fuel_combustion_consumption": {
         "Sectors": "Stationary Energy",
         "Scope": 1,
         "gpc_refno": ["I.1.1", "I.2.1", "I.3.1", "I.4.1", "I.5.1", "I.6.1"],
-        "input_values": "total_fuel_consumption",
-        "subcategories_i_j": ["fuel_type (i)", "building_type (j)"],
-        "units": ["TJ", "kg", "m3"],
-        "default_value": "emission_factor (i)",
-        "default_units": ["kg/TJ", "kg/kg", "kg/m3"],
-        "formula": "\text{emissions}_\text{i,j}=\text{activity_value}_\text{i,j}*\text{emission_factor}_\text{i}",
+        "activity_name": "fuel_combustion",
+        "units": ["TJ", "kg", "m3"]
     },
     "energy_consumption": {
         "sectors": "Stationary Energy and Transportation",
@@ -64,33 +47,21 @@ methodologies_mapping = {
             "II.4.2",
             "II.5.2",
         ],
-        "input_values": "total_fuel_consumption",
-        "subcategories_i_j": ["building_type (j)", "transport_type (j)"],
-        "units": ["kWh"],
-        "default_value": "emission_factor (i)",
-        "dafault_units": ["kg/kW"],
-        "formula": "\text{emissions}_\text{i,j}=\text{activity_value}_\text{i,j}*\text{emission_factor}_\text{i}",
+        "activity_name": "electricity_consumption",
+        "units": ["kWh"]
     },
     "fuel_sales": {
         "sectors": "Transportation",
         "scope": 1,
         "gpc_refno": ["II.1.1", "II.2.1", "II.3.1", "II.4.1", "II.5.1"],
-        "input_values": "total_fuel_sold",
-        "subcategories_i_j": ["fuel_type (i)", "transport_type (j)"],
-        "units": ["TJ", "kg", "m3"],
-        "default_value": "emission_factor (i)",
-        "default_units": ["kg/TJ", "kg/kg", "kg/m3"],
-        "formula": "\text{emissions}_\text{i,j}=\text{activity_value}_\text{i,j}*\text{emission_factor}_\text{i}",
+        "activity_name": "fuel_combustion",
+        "units": ["TJ", "kg", "m3"]
     },
     "movement_driver": {
         "sectors": "Transportation",
         "scope": 1,
         "gpc_refno": ["II.2.1"],
-        "input_values": "total_movement_driver",
-        "subcategories_i_j": ["transport_type (j)"],
-        "units": ["pkm", "gkm"],
-        "default_value": "emission_factor (j)",
-        "default_units": ["kg/pkm", "kg/gkm"],
-        "formula": "\text{emissions}_\text{i,j}=\text{activity_value}_\text{i,j}*\text{emission_factor}_\text{i}",
-    },
+        "activity_name": "vehicle_kilometers_traveled",
+        "units": ["pkm", "gkm"]
+    }
 }
