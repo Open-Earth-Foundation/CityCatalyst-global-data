abab81950af4526e848c5f70a9d4bf27fb6348e4
Amanda Eames <amanda@openearth.org>
2025-01-15 17:02:17 +1100

    fix update statement for ef

--- cc-mage/data_loaders/update_snis_emission_factor.sql
+++ cc-mage/data_loaders/update_snis_emission_factor.sql
@@ -7,7 +7,7 @@ WITH activity_data AS (
     FROM modelled.snis_staging
 )
 INSERT INTO modelled.emissions_factor 
-    (emissionfactor_id, gas_name, emissionfactor_value, unit_denominator, activity_id, datasource_name, metadata, active_from, active_to, actor_id)
+    (emissionfactor_id, gas_name, emissionfactor_value, unit_denominator, activity_id, datasource_name, active_from, active_to, actor_id)
 SELECT DISTINCT
     (MD5(CONCAT_WS('-', gas_name, emissionfactor_value, unit_denominator, activity_id::TEXT, 'IPCC 2006', 'BR'))::UUID) AS emissionfactor_id,
     gas_name,
@@ -15,7 +15,7 @@ SELECT DISTINCT
     unit_denominator,
     activity_id,
     'IPCC 2006' AS datasource_name,
-    NULL::jsonb AS metadata,
+    --NULL::jsonb AS metadata,
     NULL::DATE AS active_from,
     NULL::DATE AS active_to,
     'BR' AS actor_id
