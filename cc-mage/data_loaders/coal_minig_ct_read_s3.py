294a15c751e912c246f4d3ec4d3ac0546998a0c1
mfonseca <maureen@openearth.org>
2025-02-10 16:44:22 -0600

    feat: coal pipeline

---
+++ cc-mage/data_loaders/coal_minig_ct_read_s3.py
@@ -0,0 +1,63 @@
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.config import ConfigFileLoader
+from mage_ai.io.s3 import S3
+from os import path
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+from os import path
+import pandas as pd
+from io import BytesIO
+
+
+@data_loader
+def load_from_s3_bucket(*args, **kwargs):
+    """
+    Template for loading data from a S3 bucket.
+    Specify your configuration settings in 'io_config.yaml'.
+
+    Docs: https://docs.mage.ai/design/data-loading#s3
+    """
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
+    
+    bucket_name = 'test-global-api'
+    #bucket_name = kwargs['bucket_name']
+    route = 'raw_data/climateTRACE'
+
+    object_keys = [
+        f'{route}/coal-mining_emissions_sources_ch4.csv',
+        f'{route}/coal-mining_emissions_sources_co2.csv',
+    ]
+
+    s3 = S3.with_config(ConfigFileLoader(config_path, config_profile))
+    data_frames = []
+
+    for object_key in object_keys:
+        try:
+            file_obj = s3.client.get_object(Bucket=bucket_name, Key=object_key)['Body'].read()
+            data = pd.read_csv(BytesIO(file_obj))
+            data_frames.append(data)
+        except Exception as e:
+            print(f'Error loading {object_key}: {str(e)}')
+            continue
+
+    # Concatenate all DataFrames into one
+    combined_data = pd.concat(data_frames, ignore_index=True)
+
+    # Ensure all columns have consistent types
+    for col in combined_data.columns:
+        if combined_data[col].dtype == 'object':
+         combined_data[col] = combined_data[col].astype(str)
+
+    return combined_data
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
---
+++ cc-mage/transformers/locode_transformation_coal_ct.sql
@@ -0,0 +1,53 @@
+DROP TABLE IF EXISTS modelled.emissions_staging_full;
+
+CREATE TABLE modelled.emissions_staging_full AS
+WITH emissions_ct AS (
+    SELECT  
+        'ClimateTRACEv2024' AS datasource_name,
+        gpc_refno,
+        b.iso2_code as country_code,
+        emissions_year,
+        emissions_value,
+        emissions_units,
+        unit_denominator,
+        gas_name,
+        (MD5(CONCAT_WS('-', activity_name, activity_subcategory_type))::UUID) AS activity_id,
+        activity_name,
+        activity_subcategory_type,
+        activity_units,
+        activity_value,
+        emissionsfactor_value,
+        a.actor_id,
+        NULL::date AS active_from,
+        NULL::date AS active_to,
+        ST_GeometryType(ST_MakePoint(lon, lat)) AS geometry_type,
+        ST_MakePoint(lon, lat) AS geometry
+    FROM modelled.fugitivie_staging a
+    LEFT JOIN raw_data.country_codes b 
+	ON a.actor_id = b.iso3_code
+	GROUP BY actor_id, emissionsfactor_value, activity_value, activity_units, unit_denominator, emissions_units, emissions_value, gpc_refno, country_code, emissions_year, gas_name, activity_name, activity_subcategory_type, ST_MakePoint(lon, lat)
+)
+SELECT  
+    datasource_name,
+    country_code,
+    gpc_refno,
+    emissions_value,
+    emissions_year,
+    emissions_units,
+    (MD5(CONCAT_WS('-', gpc_refno, 'custom-methodology'))::UUID) as gpcmethod_id,
+    gas_name,
+    (MD5(CONCAT_WS('-', gas_name, emissionsfactor_value, unit_denominator, activity_id::TEXT, 'ClimateTRACEv2024', actor_id))::UUID) AS emissionfactor_id,
+    emissionsfactor_value AS emissionfactor_value,
+    unit_denominator,
+    activity_id,
+    activity_name,
+    activity_units,
+    activity_subcategory_type,
+    activity_value,
+    geometry_type,
+    geometry,
+    ST_GeoHash(geometry,20) as geometry_id
+FROM emissions_ct;
+
+CREATE INDEX IF NOT EXISTS idx_geom_temp ON modelled.emissions_staging_full USING GIST (geometry);
+DROP TABLE raw_data.country_codes;
