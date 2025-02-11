e783750e18544354cc22ac395043224f625a99c7
Amanda Eames <amanda@openearth.org>
2025-02-11 16:52:52 -0400

    feat added subcategories and region to join condition

--- cc-mage/data_loaders/update_sinir_emission_factors.sql
+++ cc-mage/data_loaders/update_sinir_emission_factors.sql
@@ -1,25 +1,26 @@
 WITH activity_data AS (
     SELECT DISTINCT
-        (MD5(CONCAT_WS('-', activity_name))::UUID) AS activity_id,
+        (MD5(CONCAT_WS('-', activity_name, activity_subcategory_type))::UUID) AS activity_id,
         gas_name,
         emissionfactor_value,
         actor_id
-    FROM modelled.sinir_staging
+    FROM raw_data.sinir_staging
 )
 INSERT INTO modelled.emissions_factor 
     (emissionfactor_id, gas_name, emissionfactor_value, unit_denominator, activity_id, datasource_name, active_from, active_to, actor_id)
 SELECT DISTINCT
-    (MD5(CONCAT_WS('-', gas_name, emissionfactor_value, 't', activity_id::TEXT, 'IPCC 2006', actor_id))::UUID) AS emissionfactor_id,
+    (MD5(CONCAT_WS('-', gas_name, emissionfactor_value, 't', activity_id::TEXT, 'IPCCv2006', actor_id))::UUID) AS emissionfactor_id,
     gas_name,
     emissionfactor_value,
     't' AS unit_denominator,
     activity_id,
-    'IPCC 2006' AS datasource_name,
+    'IPCCv2006' AS datasource_name,
     --NULL::jsonb AS metadata,
     NULL::DATE AS active_from,
     NULL::DATE AS active_to,
     actor_id
 FROM activity_data
+WHERE emissionfactor_value > 0
 ON CONFLICT (emissionfactor_id)
 DO UPDATE SET 
     gas_name = EXCLUDED.gas_name,
