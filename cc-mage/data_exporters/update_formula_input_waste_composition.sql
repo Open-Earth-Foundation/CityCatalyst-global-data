c8d4607db7b81f7ff5f461c50f0a0bbf9543614c
mfonseca <maureen@openearth.org>
2025-07-22 07:42:44 -0600

    fix: formula_name

--- cc-mage/data_exporters/update_formula_input_waste_composition.sql
+++ cc-mage/data_exporters/update_formula_input_waste_composition.sql
@@ -25,11 +25,11 @@ publisher_ids AS (
         dataset_name
     FROM modelled.publisher_datasource
 ),
--- I cannot generate the ids for the publisher and dataset because those values were already calculated using a different combination in the UUID
+-- Match with known publisher/dataset IDs
 ids_data AS (
     SELECT
         fr.*,
-        MD5(CONCAT_WS('-', methodology_name, gpc_refno))::UUID AS method_id,
+        MD5(CONCAT_WS('-', fr.methodology_name, fr.gpc_refno))::UUID AS method_id,
         pid.publisher_id,
         pid.dataset_id
     FROM formula_raw fr
@@ -49,23 +49,25 @@ INSERT INTO modelled.formula_input (
     formula_input_value,
     formula_input_units,
     metadata,
-    actor_id
+    actor_id,
+    formula_name
 )
 SELECT 
-    MD5(CONCAT_WS('-', gas, parameter_code, gpc_refno, actor_id))::UUID AS formula_input_id,
-    method_id, 
-    publisher_id, 
-    dataset_id, 
-    gas,
-    parameter_code,
-    _parameter_name as parameter_name,
-    gpc_refno as gpc_reference_number,
-    formula_input_value,
-    formula_input_units,
+    MD5(CONCAT_WS('-', fr.formula_name, fr.gas, fr.parameter_code, fr.gpc_refno, fr.actor_id))::UUID AS formula_input_id,
+    fr.method_id, 
+    fr.publisher_id, 
+    fr.dataset_id, 
+    fr.gas,
+    fr.parameter_code,
+    fr._parameter_name AS parameter_name,
+    fr.gpc_refno AS gpc_reference_number,
+    fr.formula_input_value,
+    fr.formula_input_units,
     NULL::jsonb AS metadata,
-    actor_id
-FROM ids_data
-WHERE publisher_id IS NOT NULL AND dataset_id IS NOT NULL
+    fr.actor_id,
+    fr.formula_name
+FROM ids_data fr
+WHERE fr.publisher_id IS NOT NULL AND fr.dataset_id IS NOT NULL
 ON CONFLICT (formula_input_id) DO UPDATE SET
     method_id = EXCLUDED.method_id, 
     publisher_id = EXCLUDED.publisher_id, 
@@ -77,4 +79,5 @@ ON CONFLICT (formula_input_id) DO UPDATE SET
     formula_input_value = EXCLUDED.formula_input_value,
     formula_input_units = EXCLUDED.formula_input_units,
     metadata = EXCLUDED.metadata,
-    actor_id = EXCLUDED.actor_id;
+    actor_id = EXCLUDED.actor_id,
+    formula_name = EXCLUDED.formula_name;
