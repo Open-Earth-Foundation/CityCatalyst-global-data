e7bc58cf9fdd509311f64dea8f7c1dac184a7d83
mfonseca <maureen@openearth.org>
2025-08-04 06:31:18 -0600

    fix: applying portion to co2e value

--- cc-mage/transformers/transformation_raw_electricity_maps.py
+++ cc-mage/transformers/transformation_raw_electricity_maps.py
@@ -10,6 +10,27 @@ from pandas import DataFrame
 def transform(data: DataFrame, *args, **kwargs):
     """
     """
+    # diccionary of GWP AR6 - 2021 - 100-year time period
+    GWP_dic_100years = {
+        "CO2": {"value": 1, "portion": 0.80},
+        "CH4": {"value": 29.8, "portion": 0.15},
+        "N2O": {"value": 273, "portion": 0.05},
+    }
+
+    gpc_mapping = {
+        'I.1.2': 'energy-consumption-residential-buildings-methodology',
+        'I.2.2': 'energy-consumption-commercial-buildings-methodology', 
+        'I.3.2': 'energy-consumption-manufacturing-and-construction-methodology', 
+        'I.4.2': 'energy-consumption-energy-industries-methodology', 
+        'I.5.2': 'energy-consumption-agriculture-forestry-fishing-activities-methodology', 
+        'I.6.2': 'energy-consumption-non-specific-sources-methodology', 
+        'II.1.2': 'electricity-consumption-on-road-transport-methodology', 
+        'II.2.2': 'electricity-consumption-railways-methodology', 
+        'II.3.2': 'electricity-consumption-waterborne-navigation-methodology', 
+        'II.4.2': 'electricity-consumption-aviation-methodology', 
+        'II.5.2': 'electricity-consumption-off-road-transport-methodology', 
+    }
+
     # standarization of zone ids
     data['Zone id'].replace({'CL-SEN':'CL', 'ES-CE': 'ES', 'FR-COR': 'FR', 'US-SW-PNM': 'US'}, inplace=True)
 
@@ -32,19 +53,20 @@ def transform(data: DataFrame, *args, **kwargs):
     # filter needed columns
     data = data[['region', 'actor_id', 'emissions_per_activity', 'reference', 'year', 'datasource_name']]
 
-    gpc_mapping = {
-        'I.1.2': 'energy-consumption-residential-buildings-methodology',
-        'I.2.2': 'energy-consumption-commercial-buildings-methodology', 
-        'I.3.2': 'energy-consumption-manufacturing-and-construction-methodology', 
-        'I.4.2': 'energy-consumption-energy-industries-methodology', 
-        'I.5.2': 'energy-consumption-agriculture-forestry-fishing-activities-methodology', 
-        'I.6.2': 'energy-consumption-non-specific-sources-methodology', 
-        'II.1.2': 'electricity-consumption-on-road-transport-methodology', 
-        'II.2.2': 'electricity-consumption-railways-methodology', 
-        'II.3.2': 'electricity-consumption-waterborne-navigation-methodology', 
-        'II.4.2': 'electricity-consumption-aviation-methodology', 
-        'II.5.2': 'electricity-consumption-off-road-transport-methodology', 
-    }
+    # applying portions of contribution of each gas and calculating emission values
+    new_rows = []
+    for index, row in data.iterrows():
+        total_co2e = row["emissions_per_activity"]
+
+        for gas in GWP_dic_100years.keys():
+            new_row = row.copy()
+            new_row["gas"] = gas
+            new_row["emissions_per_activity"] = (
+                GWP_dic_100years[gas]["portion"] * total_co2e
+            ) / GWP_dic_100years[gas]["value"]
+            new_rows.append(new_row)
+
+    data = pd.DataFrame(new_rows)
 
     # assign gpc_reference_number and methodology_name
     data = pd.concat([
@@ -53,7 +75,6 @@ def transform(data: DataFrame, *args, **kwargs):
     ], ignore_index=True)
 
     # adding extra columns
-    data['gas'] = 'CO2e'
     data['units'] = 'kg/kWh'
     data['activity_name'] = 'activity-energy-consumption'
     data['activity_units'] = 'kWh'
